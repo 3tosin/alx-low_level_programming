4155fe59c557a385e6c86153d89e26bffdd0fe83
3tosin <adeniyioluwafaith@gmail.com>
2022-03-15 12:30:57 -0700

    c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,14 @@
 
 /**
  * main - prints the alphabet, in lowercase, followed by a new line
- *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	char i;
-	
+
 	i = 'a';
-	
+
 	while (i <= 'z')
 	{
 		_putchar(i);

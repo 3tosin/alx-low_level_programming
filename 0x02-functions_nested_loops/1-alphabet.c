f9134d927b5299c012065452b7927f32a2fcd5e6
3tosin <adeniyioluwafaith@gmail.com>
2022-03-14 23:51:01 -0700

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char i;
+
+	i = 'a';
+	
+	while (i <= 'z')
+	{
+		_putchar('i');
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}

eaea779566c5b7f517d1fc9bbd956dbee429d961
3tosin <adeniyioluwafaith@gmail.com>
2022-03-15 10:07:11 -0700

    functions

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+	int i;
+
+	if ( n < 0)
+	{
+		n = -n;
+	}
+	
+	i = n % 10;
+	if (i < 0)
+	{
+		i = -i;
+	}
+	_putchar(i + '0');
+	return (i);
+}

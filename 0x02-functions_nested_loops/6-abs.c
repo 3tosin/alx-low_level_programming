9a4e948976aadd46c47cc59a85fa9088e1b29a54
3tosin <adeniyioluwafaith@gmail.com>
2022-03-15 13:03:52 -0700

    c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,10 +2,10 @@
 
 /**
  * _abs - computes the absolute value of an integer
- *@n: the int to check
+ *@i: the int to check
  * Return: the absolute value of int
  */
-int _abs(int i) 
+int _abs(int i)
 {
 	if (i > 0)
 	{

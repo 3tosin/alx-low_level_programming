4eb0bb2e373ac161d7c39f9ca77345af42b9bad2
3tosin <adeniyioluwafaith@gmail.com>
2022-04-16 01:42:58 -0700

    c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,6 +12,11 @@ int _atoi(char *s)
 	while (*(s + count) != '\0')
 	{
 		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 		{
 			if (size > 0)
 				m *= 10;
